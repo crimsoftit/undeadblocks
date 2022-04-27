f49fdc3e8b1af2a1a64b95918c33119406360504
dcrimsoft <dcrimsoft@gmail.com>
2022-04-27 05:28:38 +0300

    Updates

---
+++ precache-manifest.8e19a20a9bb8f3da0acfdcb921646f6f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8135cb47066b9686e34ecb46f72812b1",
+    "url": "/undeadblocks/client/index.html"
+  },
+  {
+    "revision": "310a82a0c39582f52f7f",
+    "url": "/undeadblocks/client/static/css/main.c33008af.chunk.css"
+  },
+  {
+    "revision": "bbbeb0b227d79aff4541",
+    "url": "/undeadblocks/client/static/js/2.8ba4e531.chunk.js"
+  },
+  {
+    "revision": "310a82a0c39582f52f7f",
+    "url": "/undeadblocks/client/static/js/main.6de839cc.chunk.js"
+  },
+  {
+    "revision": "0c697e1b91052b98049b",
+    "url": "/undeadblocks/client/static/js/runtime-main.f6c483f0.js"
+  },
+  {
+    "revision": "bc5dc96b3ec54e01dab2d037f8d4a674",
+    "url": "/undeadblocks/client/static/media/add_icon.bc5dc96b.PNG"
+  },
+  {
+    "revision": "0e7bc5750f71095e9d42c958aa2d0c4b",
+    "url": "/undeadblocks/client/static/media/amateur.0e7bc575.PNG"
+  },
+  {
+    "revision": "ec10d60b8143f04205fc5946bd233320",
+    "url": "/undeadblocks/client/static/media/amateur_bg.ec10d60b.PNG"
+  },
+  {
+    "revision": "3b746597dfc0e2ff775a7d0e046ef9c6",
+    "url": "/undeadblocks/client/static/media/assassin_bg.3b746597.png"
+  },
+  {
+    "revision": "308c420b6656412e73b121fbc0da67c9",
+    "url": "/undeadblocks/client/static/media/home_logo.308c420b.png"
+  },
+  {
+    "revision": "118fd46f5dd181e185ceba4b177ecd7d",
+    "url": "/undeadblocks/client/static/media/main_bg.118fd46f.jpg"
+  },
+  {
+    "revision": "fd5ab76ffffad19abd24c717890a50b9",
+    "url": "/undeadblocks/client/static/media/survivor_bg.fd5ab76f.png"
+  },
+  {
+    "revision": "086fc202889da85a17c39b1eea653636",
+    "url": "/undeadblocks/client/static/media/udb.086fc202.png"
+  },
+  {
+    "revision": "aef0822e000c7cb09be8728612b68af6",
+    "url": "/undeadblocks/client/static/media/z_killer.aef0822e.PNG"
+  }
+]);
